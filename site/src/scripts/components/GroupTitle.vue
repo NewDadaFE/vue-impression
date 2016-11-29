b5cee1e7f10ce584fdd4dd5546f86201597c3ae5
peter <peter.shenlq@gmail.com>
2016-11-29 14:57:11 +0800

    add: GrouptTitle

---
+++ site/src/scripts/components/GroupTitle.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="group-title">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GroupTitle',
+    };
+</script>
--- site/src/scripts/views/index.vue
+++ site/src/scripts/views/index.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
-        <div>title</div>
-        <group class="no-margin" v-for="group in groups">
-            <cell v-for="cell in group.children">{{cell.name}}</cell>
-        </group>
-                <div>title</div>
-        <group class="no-margin" v-for="group in groups">
-            <cell v-for="cell in group.children">{{cell.name}}</cell>
-        </group>
+        <div v-for="group in groups">
+            <group-title v-if="group.title">{{group.title}}</group-title>
+            <group>
+                <cell v-for="cell in group.children">{{cell.name}}</cell>
+            </group>
+        </div>
     </div>
 </template>
 
 
 <script>
     import Group from '../components/Group';
+    import GroupTitle from '../components/GroupTitle';
     import Cell from '../components/Cell';
     import routesConfig from '../routes.json';
 
     export default {
         components: {
             Group,
+            GroupTitle,
             Cell,
         },
         data() {
