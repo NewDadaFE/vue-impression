3d22c1d554317692685148d6b3afb4e2cf8ec1d1
peter <peter.shenlq@gmail.com>
2016-12-06 17:28:58 +0800

    improve: Tabar组件属性抽离为单独mixins

--- site/src/scripts/views/tabbar.vue
+++ site/src/scripts/views/tabbar.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
-        <tabbar v-model="newsType" class="no-margin">
-            <tabbar-item value="1">体育</tabbar-item>
-            <tabbar-item value="2">财经</tabbar-item>
-            <tabbar-item value="3">科技</tabbar-item>
-            <tabbar-item value="4">军事</tabbar-item>
+        <group-title>base</group-title>
+        <tabbar class="no-margin" @change="changeHandle">
+            <tabbar-item>Sports</tabbar-item>
+            <tabbar-item>Finance</tabbar-item>
+            <tabbar-item>Science</tabbar-item>
+            <tabbar-item>Military</tabbar-item>
         </tabbar>
-        <group-title>Dynamic</group-title>
-        <tabbar v-model="tab" class="no-margin">
-            <tabbar-item v-for="item in tabs" :value="item.value">{{item.name}}</tabbar-item>
+        <group-title>activeKey="3"</group-title>
+        <tabbar class="no-margin" activeKey="Science" @change="changeHandle">
+            <tabbar-item eventKey="Sports">Sports</tabbar-item>
+            <tabbar-item eventKey="Finance">Finance</tabbar-item>
+            <tabbar-item eventKey="Science">Science</tabbar-item>
+            <tabbar-item eventKey="Military">Military</tabbar-item>
+        </tabbar>
+        <group-title>dynamic</group-title>
+        <tabbar class="no-margin" activeKey="Finance" @change="changeHandle">
+            <tabbar-item v-for="item in tabs" :eventKey="item.name">{{item.name}}</tabbar-item>
         </tabbar>
     </div>
 </template>
@@ -17,22 +25,21 @@
     export default {
         data() {
             return {
-                newsType: '1',
-                tab: '1',
                 tabs: [{
-                    name: 'Overview',
-                    value: '1',
+                    name: 'Sports',
                 }, {
-                    name: 'Stars',
-                    value: '2',
+                    name: 'Finance',
                 }, {
-                    name: 'Followers',
-                    value: '3',
+                    name: 'Science',
                 }, {
-                    name: 'Following',
-                    value: '4',
+                    name: 'Military',
                 }],
             };
+        },
+        methods: {
+            changeHandle(val) {
+                this.$toast(`选择了:${val}`);
+            },
         },
     };
 </script>
