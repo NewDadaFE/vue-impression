dfc3820c7d59b13588aa91efb26edba275cc4304
peter <peter.shenlq@gmail.com>
2016-12-06 11:50:39 +0800

    improve: 抽离Selector和SelectorOption下的配置项成为单独的mixins，便于InlineSelector组件复用

--- site/src/scripts/components/Selector.vue
+++ site/src/scripts/components/Selector.vue
@@ -6,35 +6,10 @@
 
 <script>
     import Sync from '../mixins/sync';
+    import Select from '../mixins/select';
 
     export default {
         name: 'selector',
-        mixins: [Sync],
-        props: {
-            disabled: Boolean,
-            multiple: Boolean,
-        },
-        methods: {
-            optionClickHandle(option) {
-                if(this.disabled) return;
-
-                if(this.multiple) {
-                    let index = this.currentValue.indexOf(option.value);
-
-                    if(index === -1) {
-                        this.currentValue.push(option.value);
-                    } else {
-                        this.currentValue.splice(index, 1);
-                    }
-
-                    return;
-                }
-
-                this.currentValue = option.value;
-            },
-        },
-        created() {
-            this.$on('optionClick', this.optionClickHandle);
-        },
+        mixins: [Sync, Select],
     };
 </script>
