856cd3470b172cb6fee9c4f0b168d71a4bcc83e2
peter <peter.shenlq@gmail.com>
2016-12-07 09:56:46 +0800

    update: InlineSelector demo

--- site/src/scripts/views/inline-selecor.vue
+++ site/src/scripts/views/inline-selecor.vue
@@ -31,6 +31,21 @@
             </cell>
         </group>
         <group-title>
+            disabled
+            <span class="pull-right">size: {{multiple}}</span>
+        </group-title>
+        <group>
+            <cell>
+                <inline-selector v-model="multiple" multiple @change="changeHandle">
+                    <inline-selector-option value="xxl">XXL</inline-selector-option>
+                    <inline-selector-option disabled value="m">M</inline-selector-option>
+                    <inline-selector-option value="s">S</inline-selector-option>
+                    <inline-selector-option disabled value="l">L</inline-selector-option>
+                    <inline-selector-option disabled value="xl">XL</inline-selector-option>
+                </inline-selector>
+            </cell>
+        </group>
+        <group-title>
             theme: 'danger'
             <span class="pull-right">size: {{theme}}</span>
         </group-title>
@@ -40,7 +55,7 @@
                     <inline-selector-option value="xxl">XXL</inline-selector-option>
                     <inline-selector-option value="m">M</inline-selector-option>
                     <inline-selector-option value="s">S</inline-selector-option>
-                    <inline-selector-option disabled value="l">L</inline-selector-option>
+                    <inline-selector-option value="l">L</inline-selector-option>
                     <inline-selector-option value="xl">XL</inline-selector-option>
                 </inline-selector>
             </cell>
