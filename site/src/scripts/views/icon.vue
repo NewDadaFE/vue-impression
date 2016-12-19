3c1c340c1bd6a0c25bc29cd24f6fb3bb0572aae8
peter <peter.shenlq@gmail.com>
2016-12-19 11:20:20 +0800

    improve: Icon

--- site/src/scripts/views/icon.vue
+++ site/src/scripts/views/icon.vue
@@ -1,7 +1,31 @@
 <template>
     <div>
+        <group-title>fontawesome</group-title>
         <group>
             <cell href="http://fontawesome.io/icons/">Fontawesome</cell>
+        </group>
+        <group-title>icons</group-title>
+        <group>
+            <cell>
+                <icon name="bell" left />
+                <icon name="car" left />
+                <icon name="camera" left />
+            </cell>
+            <cell>
+                <icon name="bell" left size="lg" />
+                <icon name="car" left size="lg" />
+                <icon name="camera" left size="lg" />
+            </cell>
+            <cell>
+                <icon name="bell" left size="2x" />
+                <icon name="car" left size="2x" />
+                <icon name="camera" left size="2x" />
+            </cell>
+            <cell>
+                <icon name="bell" left size="lg" class="text-danger" />
+                <icon name="car" left size="lg" class="text-danger" />
+                <icon name="camera" left size="lg" class="text-danger" />
+            </cell>
         </group>
     </div>
 </template>
