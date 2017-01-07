dc0f727985da0db86933b804da67bf061c286c70
peter <peter.shenlq@gmail.com>
2017-01-07 18:33:07 +0800

    [FIX]: 调整字体大小变化规则，只有在IOS设备下才将字体改为14px;修正IOS下textarea字体太小

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -122,7 +122,9 @@ const install = Vue => {
     if(install.installed) return;
 
     // 视网膜屏
-    if(window.devicePixelRatio && window.devicePixelRatio >= 2) {
+    let userAgent = navigator.userAgent.toLowerCase();
+
+    if(/iphone|ipad|ipod/.test(userAgent)) {
         document.documentElement.style.fontSize = '14px';
     }
 
