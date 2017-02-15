0e06ff41a3f79e53205988aa7c4dba6608db6e8f
peter <peter.shenlq@gmail.com>
2017-02-15 13:58:59 +0800

    [REMOVE]: App.vue

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import router from './router';
-import App from './App';
 import Impression from './components';
 
 Vue.use(Impression);
@@ -8,6 +7,6 @@ Vue.use(Impression);
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    render: h => h(App),
     router,
+    template: '<router-view></router-view>',
 });
