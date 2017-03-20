ecc8c4eb502e44deb3375e480e6f0d56f3c3ba39
yonglinhuang <yonglinhuang@imdada.cn>
2017-03-20 10:38:32 +0800

    [FIX]: Media组件 1. props属性align center值 => middle 2. class中get method => 字符串模板

--- src/scripts/components/Media.vue
+++ src/scripts/components/Media.vue
@@ -2,7 +2,7 @@
     <div
         @click="$emit('click')"
         class="media"
-        :class="[getAlignClass()]">
+        :class="`flex-align-${align}`">
         <slot></slot>
     </div>
 </template>
@@ -16,17 +16,8 @@
                 type: String,
                 default: 'top',
                 validator(value) {
-                    return ['top', 'bottom', 'center'].indexOf(value) > -1;
+                    return ['top', 'bottom', 'middle'].indexOf(value) > -1;
                 },
-            },
-        },
-        methods: {
-            getAlignClass() {
-                return {
-                    top: 'flex-align-top',
-                    center: 'flex-align-middle',
-                    bottom: 'flex-align-bottom',
-                }[this.align];
             },
         },
     };
