de4cff54068856d3c9626daf99fcc1cc8cfaaac8
yonglinhuang <yonglinhuang@imdada.cn>
2017-03-20 10:32:17 +0800

    [FIX] Badge class 字符串相加 => 字符串模板

--- src/scripts/components/Badge.vue
+++ src/scripts/components/Badge.vue
@@ -3,7 +3,7 @@
         @click="$emit('click')"
         class="badge"
         :class="{'badge-gap': $slots.default}">
-        <div class="badge-addon" :class="'bg-' + theme">{{content}}</div>
+        <div class="badge-addon" :class="`bg-${theme}`">{{content}}</div>
         <slot></slot>
     </div>
 </template>
