1adf4ea304535f9c9ecb44d442f40616d244ad99
peter <peter.shenlq@gmail.com>
2017-05-04 17:56:47 +0800

    [IMPROVE]: Alert组件添加全局hide方法。

--- src/scripts/utils/alert.js
+++ src/scripts/utils/alert.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import OriginAlert from '../components/Alert';
 
 const Alert = Vue.extend(OriginAlert);
+let alertInstance;
 
 // Alert框
 export const alert = option => {
     option.title = option.title || '提示';
 
-    let alertInstance = new Alert({
+    alertInstance = new Alert({
         el: document.createElement('div'),
     });
 
@@ -18,4 +19,10 @@ export const alert = option => {
     Vue.nextTick(() => {
         alertInstance.show();
     });
+
+    return alertInstance;
+};
+
+alert.hide = () => {
+    alertInstance.hide();
 };
